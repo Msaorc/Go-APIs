3ca2883e1c666a9ff265aac94820736e43153542
Marcos Augusto <msaorc@hotmail.com>
2023-02-08 18:01:21 -0300

    Finalizing the product endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/Msaorc/Go-APIs/configs"
-	"github.com/Msaorc/Go-APIs/internal/dto"
 	"github.com/Msaorc/Go-APIs/internal/entity"
+	"github.com/Msaorc/Go-APIs/internal/handlers"
 	"github.com/Msaorc/Go-APIs/internal/infra/database"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -22,38 +21,8 @@ func main() {
 		panic(err)
 	}
 	db.AutoMigrate(&entity.User{}, &entity.Product{})
-	productHandler := NewProductHandler(database.NewProduct(db))
+	productHandler := handlers.NewProductHandler(database.NewProduct(db))
 	mux := http.NewServeMux()
 	mux.HandleFunc("/products", productHandler.CreateProduct)
 	http.ListenAndServe(":8081", mux)
-}
-
-type ProductHandler struct {
-	ProductDB database.ProductInterface
-}
-
-func NewProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductInput
-	err := json.NewDecoder(r.Body).Decode(&product)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	p, err := entity.NewProduct(product.Name, product.Price)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	err = h.ProductDB.Create(p)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
 }
